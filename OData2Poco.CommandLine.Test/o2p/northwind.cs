d326ca1422aa7c8deb16cfb76db4e8c4d30e4934
moh-hassan <moh.hassan20@gmail.com>
2016-05-06 00:54:05 +0200

    add namespace for generated code northwin.cs

--- OData2Poco.CommandLine.Test/o2p/northwind.cs
+++ OData2Poco.CommandLine.Test/o2p/northwind.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Spatial;
 
 //uncomment for spatial data type and run Install-Package System.Spatial  
 //using System.Spatial; 
