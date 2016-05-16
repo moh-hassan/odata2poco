5ea8ef1c3c4c48ed9c4e4244cb86a7e147eed3af
moh-hassan <moh.hassan20@gmail.com>
2016-05-16 22:37:14 +0200

    fluent1

---
+++ OData2Poco.Tests/OData2PocoTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace OData2Poco.Tests
+{
+
+    [TestFixture]
+    class OData2PocoTest
+    {
+        const string UrlV4 = "http://services.odata.org/V4/Northwind/Northwind.svc";
+        const string UrlV3 = "http://services.odata.org/V3/Northwind/Northwind.svc";
+      
+        [Test]
+        public void GenerateTest()
+        {
+            var code = new O2P()
+                .SetUrl("http://services.odata.org/V4/Northwind/Northwind.svc")
+                .AddKeyAttribute()
+                .AddTableAttribute()
+                .Generate();
+            Console.WriteLine(code);
+        }
+        [Test]
+        public void GenerateTest3()
+        {
+            var code = new O2P()
+                .SetUrl("http://services.odata.org/V4/Northwind/Northwind.svc")
+                .SetUser("user")
+                .SetPassword("pw")
+                .AddKeyAttribute()
+                .AddRequiredAttribute()
+                .AddNavigation()
+                .AddTableAttribute()
+                .Generate();
+            Console.WriteLine(code);
+        }
+
+        [Test]
+        public void GenerateTest2()
+        {
+            var code = new O2P()
+               // .SetUrl("http://services.odata.org/V4/Northwind/Northwind.svc")
+                .AddKeyAttribute()
+                .AddTableAttribute()
+                .Generate();
+            Console.WriteLine(code);
+        }
+    }
+}
