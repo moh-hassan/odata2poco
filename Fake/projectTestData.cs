7a950673e7cdfa0b720411b8d5bc97133b0ee24c
Moh.Hassan <moh.hassan20@gmail.com>
2019-11-18 13:30:13 +0200

    Add tests for project generator

---
+++ Fake/projectTestData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+public static class ProjectTestData
+{
+    public static string BaseDirectory
+    {
+        get
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var codebase = new Uri(assembly.CodeBase);
+            var path = codebase.LocalPath;
+            return Path.GetDirectoryName(path);
+        }
+    }
+
+    private static readonly string RelativeFakeFolder = Path.Combine("..", "..", "..", "..", "Fake", "project_samples");
+
+    public static string FakeFolder => Path.GetFullPath(Path.Combine(BaseDirectory, RelativeFakeFolder));
+
+    public static string Generate_default_project_Test => GetFullPath("Generate_default_project_Test.txt");
+    public static string Generate_project_for_json_Test => GetFullPath("Generate_project_for_json_Test.txt");
+
+    public static string Generate_project_for_attributes_Test => GetFullPath("Generate_project_for_attributes_Test.txt");
+
+    public static string GetFullPath(string relative)
+    {
+        var path = Path.GetFullPath(Path.Combine(BaseDirectory, RelativeFakeFolder, relative));
+        return path;
+    }
+}
+
