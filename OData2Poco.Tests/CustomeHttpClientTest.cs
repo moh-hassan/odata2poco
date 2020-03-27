c02ee769d454b60f1b095fc4a2be498e3377d8fe
moh-hassan <moh.hassan20@gmail.com>
2020-03-28 01:39:18 +0200

    Add support to symbolic package snupkg and SourceLink

--- OData2Poco.Tests/CustomeHttpClientTest.cs
+++ OData2Poco.Tests/CustomeHttpClientTest.cs
@@ -7,7 +7,7 @@ namespace OData2Poco.Tests
     class CustomeHttpClientTest
     {
         [Test]
-        [TestCase(TestSample.UrlOdataV4)]
+        [TestCase(TestSample.UrlNorthWindV4)]
         [TestCase(TestSample.UrlOdataV3)]
         public async Task No_auth_ReadMetaDataTest(string url)
         {
