9802b9b0d28a2d08f98fff98023ddbd939fb24d3
moh-hassan <moh.hassan20@gmail.com>
2022-11-19 05:55:24 +0200

    Add ErrorCollection

---
+++ OData2PocoLib/ErrorCollection.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (c) Mohamed Hassan & Contributors. All rights reserved. See License.md in the project root for license information.
+
+using System.Collections;
+using OData2Poco.InfraStructure.Logging;
+
+//namespace OData2Poco.CommandLine;
+namespace OData2Poco;
+
+public class ErrorCollection : IEnumerable<OptionError>
+{
+    private const int INFO = 0;
+    private const int WARNING = 1;
+    private const int ERROR = 2;
+
+    private readonly List<OptionError> _errors = new();
+    private readonly ILog _logger = PocoLogger.Default;
+    public void Add(OptionError error)
+    {
+        _errors.Add(error);
+    }
+    public void Add(string message, int level = 0)
+    {
+        var error = new OptionError(message, level);
+        Add(error);
+    }
+    public void AddError(string message) => Add(message, ERROR);
+    public void AddWarning(string message) => Add(message, WARNING);
+    public void AddInfo(string message) => Add(message);
+
+    public int ShowErrors()
+    {
+        if (!_errors.Any()) return 0;
+        foreach (var error in _errors.OrderBy(e => e.Level))
+        {
+            switch (error.Level)
+            {
+                case 2:
+                    _logger.Error(error.Message);
+                    break;
+                case 0:
+                    _logger.Info(error.Message);
+                    break;
+                case 1:
+                    _logger.Warn(error.Message);
+                    break;
+                default:
+                    _logger.Info(error.Message);
+                    break;
+            }
+        }
+        return _errors.Any(e => e.Level >= 2) ? 1 : 0;
+    }
+    public IEnumerator<OptionError> GetEnumerator()
+    {
+        return _errors.GetEnumerator();
+
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
---
+++ OData2PocoLib/OptionError.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Mohamed Hassan & Contributors. All rights reserved. See License.md in the project root for license information.
+
+namespace OData2Poco;
+
+public class OptionError
+{
+    public string Message { get; set; }
+    public int Level { get; set; }
+
+    public OptionError(string message, int level)
+    {
+        Message = message;
+        Level = level;
+    }
+    public OptionError(string message) : this(message, 0)
+    {
+    }
+    public override string ToString()
+    {
+        return Message;
+    }
+}
